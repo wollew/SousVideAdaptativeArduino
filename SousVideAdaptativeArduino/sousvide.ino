01dae4dd9fe351393a3ef4fd330b05d589dad9ca
Wolfgang Miller-Reichling <wolfgang@miller-reichling.de>
2017-01-11 14:17:39 +0100

    enable OTA updates

--- SousVideAdaptativeArduino/sousvide.ino
+++ SousVideAdaptativeArduino/sousvide.ino
@@ -67,6 +67,9 @@
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 // ------------------------- CONSTANTS
@@ -212,17 +215,26 @@ void setup() {
     wifiManager.autoConnect("sous-vide");
     setupWeb();
 
-    delay(3000);
+    ArduinoOTA.onStart([]() { Serial.println("Start"); });
+    ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
 }
 
-
-
-/**************************************************************************************/
 /*                                                                                    */
 /*                                      MAIN LOOP                                     */
 /*                                                                                    */
 /**************************************************************************************/
-
 
 void loop() {
 
@@ -410,6 +422,7 @@ void loop() {
 
     // handle web request
     server.handleClient();
+    ArduinoOTA.handle();
 }
 
 
